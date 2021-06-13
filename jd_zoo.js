00862cd476733234476a850f9131a27db6a538c3
danteswx <50689514+danteswx@users.noreply.github.com>
2021-06-13 19:40:42 +0800

    Update jd_zoo.js

--- jd_zoo.js
+++ jd_zoo.js
@@ -21,7 +21,8 @@ const pKHelpAuthorFlag = true;//是否助力作者PK  true 助力，false 不助
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [];
 $.cookie = '';
-$.inviteList = [];
+$.inviteList = ['ZXTKT0225KkcR0pI9lDVdRnznKYKJwFjRWn6-7zx55awQ',
+'ZXTKT0225KkcRRsY8lLfch-hwvIJIgFjRWn6-7zx55awQ'];
 $.pkInviteList = [];
 $.secretpInfo = {};
 $.innerPkInviteList = [];
