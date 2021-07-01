4863a5400ec9318f8ed17f8d5456222e2608b9e0
danteswx <50689514+danteswx@users.noreply.github.com>
2021-07-01 09:14:53 +0800

    Update jd_dreamFactory.js

--- jd_dreamFactory.js
+++ jd_dreamFactory.js
@@ -44,7 +44,7 @@ const jxOpenUrl = `openjd://virtual?params=%7B%20%22category%22:%20%22jump%22,%2
 let cookiesArr = [], cookie = '', message = '', allMessage = '';
 const inviteCodes = [];
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
-$.tuanIds = [];
+$.tuanIds = ['T_zZaWP6by9yA1wehxM4mg=='];
 $.appId = 10001;
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
