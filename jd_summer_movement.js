445a71940b4b927ad2a381ed5fd9b58c04266c26
danteswx <50689514+danteswx@users.noreply.github.com>
2021-07-14 13:10:30 +0800

    Update jd_summer_movement.js

--- jd_summer_movement.js
+++ jd_summer_movement.js
@@ -21,7 +21,7 @@ const DATA = {appid:'50085',sceneid:'OY217hPageh5'};
 const ua = `jdltapp;iPhone;3.1.0;${Math.ceil(Math.random()*4+10)}.${Math.ceil(Math.random()*4)};${randomString(40)}`;
 let smashUtils;
 class MovementFaker {
-  constructor(cookie) {this.cookie = cookie;this.ua = require('./USER_AGENTS.js').USER_AGENT;}
+  constructor(cookie) {this.cookie = cookie;this.ua = ua;}
   async run() {if (!smashUtils) {await this.init();}
     var t = Math.floor(1e7 + 9e7 * Math.random()).toString();
     var e = smashUtils.get_risk_result({id: t,data: {random: t}}).log;
