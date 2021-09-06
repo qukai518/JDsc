bd88634922448b4f5d488837ceab509212d1589b
danteswx <dantes.wx@gmail.com>
2021-09-07 00:23:45 +0800

    更新

--- jd_nzmh.js
+++ jd_nzmh.js
@@ -212,7 +212,7 @@ function taskUrl(function_id, body = '') {
       'Accept-Language': 'zh-cn',
       'Content-Type': 'application/json;charset=utf-8',
       'Origin': 'wq.jd.com',
-      'User-Agent': 'JD4iPhone/167490 (iPhone; iOS 14.2; Scale/3.00)',
+      'User-Agent': 'JD4iPhone/167802 (iPhone; iOS 15.0; Scale/3.00)',
       'Referer': `https://anmp.jd.com/babelDiy/Zeus/xKACpgVjVJM7zPKbd5AGCij5yV9/index.html?wxAppName=jd`,
       'Cookie': cookie
     }
