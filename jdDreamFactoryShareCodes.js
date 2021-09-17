1dcac4547e2d0ab37e8dcea528437d83675b78b5
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2021-09-17 10:18:01 +0800

    Update jdDreamFactoryShareCodes.js

--- jdDreamFactoryShareCodes.js
+++ jdDreamFactoryShareCodes.js
@@ -7,11 +7,8 @@
 // github action用户的好友互助码填写到Action->Settings->Secrets->new Secret里面(Name填写 DREAM_FACTORY_SHARE_CODES(此处的Name必须按此来写,不能随意更改),内容处填写互助码,填写规则如下)
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
-let shareCodes = [
-  'p15jzNJ363IRRYrz5uiijQ==@kMVrN_EU0rOG1TiQJoSHjg==',
-'p15jzNJ363IRRYrz5uiijQ==@kMVrN_EU0rOG1TiQJoSHjg=='
-]
-// 判断github action里面是否有东东萌宠互助码
+let shareCodes = ['']
+// 判断github action里面是否有京喜工厂互助码 
 if (process.env.DREAM_FACTORY_SHARE_CODES) {
   if (process.env.DREAM_FACTORY_SHARE_CODES.indexOf('&') > -1) {
     console.log(`您的互助码选择的是用&隔开\n`)
@@ -23,7 +20,7 @@ if (process.env.DREAM_FACTORY_SHARE_CODES) {
     shareCodes = process.env.DREAM_FACTORY_SHARE_CODES.split();
   }
 } else if (process.env.DREAM_FACTORY_SHARE_CODES) {
-  console.log(`由于您secret里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
+  console.log(`您secret里面未提供助力码，优先进行自己账号内互助，然后再给脚本内置的码进行助力，请知晓！`)
 }
 for (let i = 0; i < shareCodes.length; i++) {
   const index = (i + 1 === 1) ? '' : (i + 1);
