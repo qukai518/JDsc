627da10a3b341df128766966461df49b043f49c6
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2021-09-17 10:21:22 +0800

    Update jdPlantBeanShareCodes.js

--- jdPlantBeanShareCodes.js
+++ jdPlantBeanShareCodes.js
@@ -7,10 +7,7 @@
 // github action用户的好友互助码填写到Action->Settings->Secrets->new Secret里面(Name填写 PLANT_BEAN_SHARECODES(此处的Name必须按此来写,不能随意更改),内容处填写互助码,填写规则如下)
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
-let PlantBeanShareCodes = [
-  '4npkonnsy7xi2ouj27jjt2ojrnpxtfou4dwquba@mlrdw3aw26j3xynofsgbd276hgnu5dt3kdie5hq',
-'4npkonnsy7xi2ouj27jjt2ojrnpxtfou4dwquba@mlrdw3aw26j3xynofsgbd276hgnu5dt3kdie5hq'
-]
+let PlantBeanShareCodes = []
 // 判断github action里面是否有东东萌宠互助码
 if (process.env.PLANT_BEAN_SHARECODES) {
   if (process.env.PLANT_BEAN_SHARECODES.indexOf('&') > -1) {
@@ -23,7 +20,7 @@ if (process.env.PLANT_BEAN_SHARECODES) {
     PlantBeanShareCodes = process.env.PLANT_BEAN_SHARECODES.split();
   }
 } else if (process.env.JD_COOKIE) {
-  console.log(`由于您secret里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
+  console.log(`您secret里面未提供助力码，优先进行自己账号内互助，然后再给脚本内置的码进行助力，请知晓！`)
 }
 for (let i = 0; i < PlantBeanShareCodes.length; i++) {
   const index = (i + 1 === 1) ? '' : (i + 1);
