837f297c2086adfbd4c618d08d03d35311063494
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2021-09-25 16:33:30 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -45,7 +45,7 @@
 # 特物Z
 30 18,19 * * * node /scripts/jd_superBrand.js >> /scripts/logs/jd_superBrand.log 2>&1
 # 集魔方
-30 18,19 * * * node /scripts/jd_mofang.js >> /scripts/logs/jd_mofang.log 2>&1
+10 5,7 * * * node /scripts/jd_mofang.js >> /scripts/logs/jd_mofang.log 2>&1
  
 
 ##############长期活动##############
