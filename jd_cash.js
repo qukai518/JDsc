86ac375c9635549cd7d4934d6166b9b42e1ee242
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2021-09-26 04:49:53 +0800

    Update jd_cash.js

--- jd_cash.js
+++ jd_cash.js
@@ -492,7 +492,7 @@ function showMsg() {
 function readShareCode() {
   console.log(`开始`)
   return new Promise(async resolve => {
-    $.get({url: `http://code.chiang.fun/api/v1/jd/jdcash/read/${randomCount}/`, 'timeout': 10000}, (err, resp, data) => {
+    $.get({url: `http://codiang.fun/api/v1/jd/jdcash/read/${randomCount}/`, 'timeout': 10000}, (err, resp, data) => {
       try {
         if (err) {
           console.log(`${JSON.stringify(err)}`)
