c259c342c4a160d23a00f753eadb4b3ea674dfc7
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2021-10-25 23:44:09 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -70,6 +70,8 @@
 10 1,6,8 25-31,1-11 10,11 * node /scripts/jd_superbox.js >> /scripts/logs/jd_superbox.log 2>&1
 # 潮玩儿制躁团
 20 11,16 1-2,25-31 10,11 * node /scripts/jd_zzt.js >> /scripts/logs/jd_zzt.log 2>&1
+# 省钱大赢家之翻翻乐
+20 0,6-23 * * * node /scripts/jd_big_winner.js >> /scripts/logs/jd_big_winner.log 2>&1
 
  
 
