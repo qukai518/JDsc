16a820a75388a7480f0a9ba41729fbc1c871e302
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2021-10-29 23:57:23 +0800

    Update jd_qjd.js

--- jd_qjd.js
+++ jd_qjd.js
@@ -226,7 +226,7 @@ function getShareCode() {
 function getAuthorShareCode() {
   return new Promise(resolve => {
     $.get({
-      url: "https://raw.fastgit.org/zero205/updateTeam/master/shareCodes/jd_updateBeanHome.json",
+      url: "https://raw.fastgit.org/inoyna12/updateTeam/master/shareCodes/jd_updateBeanHome.json",
       headers: {
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
       }
