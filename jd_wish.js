e03db0297c28b973637da127faba6ef360f1b752
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2021-11-02 19:32:31 +0800

    Update jd_wish.js

--- jd_wish.js
+++ jd_wish.js
@@ -25,8 +25,8 @@ let message = '', allMessage = '';
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '';
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-let appIdArr = ['1FV1ZwKY','1FFVQyqw','1FFRWxaY','1E1xZy6s','1FV1VwKc','1E1NXxq0'];
-let appNameArr = ['好物好生活','有1111点新动','送你超能力','PLUS生活特权','惊喜大作战','众筹许愿池'];
+let appIdArr = ['1FFRWwqg','1FV1ZwKY','1FFVQyqw','1FFRWxaY','1E1xZy6s','1FV1VwKc','1E1NXxq0'];
+let appNameArr = ['心动电波','好物好生活','有1111点新动','送你超能力','PLUS生活特权','惊喜大作战','众筹许愿池'];
 let appId, appName;
 $.shareCode = [];
 if ($.isNode()) {
