d82275c50a0a020eeb72086dd2d88ee7b2246d96
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2021-12-17 23:41:11 +0800

    Update jd_notifyJDsc.js

--- jd_notifyJDsc.js
+++ jd_notifyJDsc.js
@@ -29,7 +29,7 @@ const notify = $.isNode() ? require('./sendNotify') : '';
     $.done();
   })
 
-function getParams(url='https://raw.githubusercontent.com/inoyna12/ccgo/master/notify.json') {
+function getParams(url='https://raw.fastgit.org/inoyna12/ccgo/master/notify.json') {
   return new Promise(async resolve => {
     const options = {
       url: `${url}?${new Date()}`, "timeout": 10000, headers: {
